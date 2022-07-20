b9a905e7eb429cc5a688625b5e8b7ec34ab28758
maclux <tim@geohei.lu>
2022-07-20 16:57:34 +0200

    base
    Base bot for simple commands

---
+++ deploy-commands.js
@@ -0,0 +1,30 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+// Import environment variables
+require('dotenv').config();
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.token);
+
+/* Remove all commands globally
+rest.put(Routes.applicationCommands(process.env.clientId), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
+	.catch(console.error);
+*/
+
+// Add commands
+rest.put(Routes.applicationCommands(process.env.clientId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
---
+++ index.js
@@ -0,0 +1,52 @@
+// register commands
+// require('./deploy-commands');
+
+// Import environment variables
+require('dotenv').config();
+
+// Require the necessary discord.js classes
+const { Client, Collection } = require('discord.js');
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+// Create a new client instance
+const client = new Client({ intents: ['Guilds', 'GuildVoiceStates'] });
+
+// Create a new collection to hold the commands
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+    if (interaction.commandType !== 1) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    }
+    catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+
+// Login to Discord with your client's token
+client.login(process.env.token);
