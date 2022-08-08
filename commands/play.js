0e2491b950ef82a7a88d65c461c1b2b89aab4ab2
maclux <tim@geohei.lu>
2022-08-08 15:06:27 +0200

    fix: embed refresh song add
    Embed was not refreshing when a single song was added

--- commands/play.js
+++ commands/play.js
@@ -147,6 +147,7 @@ const addToQueue = (guild, videoId, playlistId, db) => {
             }
 
             db.push(`server.${guild.id}.music.queue`, videoInfo);
+            utils.refreshMusicEmbed(db, guild);
             resolve();
         }
     });
