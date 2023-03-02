5cdb273ee02ad4b5a7807998699d746be2bfc3cb
maclux <tim@geohei.lu>
2023-03-02 13:32:55 +0100

    Fix ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,11 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const utils = require('../utils');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with the latency!'),
-    async execute(client, interaction) {
-        await interaction.reply(`Pong! Latency is ${interaction.client.ws.ping}ms.`);
+    async execute(client, interaction, db, message) {
+        utils.reply(interaction, message?.channel, `Pong! Latency is ${client.ws.ping}ms.`);
     },
 };
