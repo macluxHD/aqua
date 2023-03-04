65b3be3155f43d58f53cca4df6e39913a153c4f5
maclux <tim@geohei.lu>
2023-03-04 17:15:34 +0100

    Fix clearqueue

--- commands/clearqueue.js
+++ commands/clearqueue.js
@@ -13,11 +13,11 @@ module.exports = {
         const guild = !interaction ? message.guild : interaction.guild;
 
         if (!getVoiceConnection(guild.id)?._state?.subscription?.player) {
-            prisma.queue.deleteMany({ where: { guildId: guild.id } });
+            await prisma.queue.deleteMany({ where: { guildId: guild.id } });
         }
         else {
             const queue = await prisma.queue.findMany({ where: { guildId: guild.id } });
-            prisma.queue.deleteMany({ where: { guildId: guild.id, NOT: { id: queue[0].id } } });
+            await prisma.queue.deleteMany({ where: { guildId: guild.id, NOT: { id: queue[0].id } } });
         }
 
         utils.reply(interaction, message?.channel, 'Cleared the Music Queue!');
