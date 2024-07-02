a400fe721734d6d79a7654c304712d071da19bd0
maclux <tim@geohei.lu>
2024-07-02 10:52:15 +0200

    refactor: upgrade packages

--- commands/play.js
+++ commands/play.js
@@ -114,20 +114,6 @@ module.exports = {
             adapterCreator: voiceChannel.guild.voiceAdapterCreator,
         });
         connection.subscribe(player);
-
-        // workaround for https://github.com/discordjs/discord.js/issues/9185
-        connection.on('stateChange', (oldState, newState) => {
-            const oldNetworking = Reflect.get(oldState, 'networking');
-            const newNetworking = Reflect.get(newState, 'networking');
-
-            const networkStateChangeHandler = (oldNetworkState, newNetworkState) => {
-                const newUdp = Reflect.get(newNetworkState, 'udp');
-                clearInterval(newUdp?.keepAliveInterval);
-            };
-
-            oldNetworking?.off('stateChange', networkStateChangeHandler);
-            newNetworking?.on('stateChange', networkStateChangeHandler);
-        });
     },
 };
 
